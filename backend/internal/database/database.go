f8b133fd241399be98cd26e481d73ed1c35a793f
cotugeh <kotugeh@gmail.com>
2026-08-11 00:54:32 +0300

    Use database SSL mode from environment

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -12,13 +12,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
+	sslMode := os.Getenv("DB_SSLMODE")
+	if sslMode == "" {
+		sslMode = "disable"
+	}
+
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		sslMode,
 	)
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -27,4 +33,5 @@ func ConnectDB() {
 	}
 
 	DB = database
+	log.Println("Connected to database successfully")
 }
